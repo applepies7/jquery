4553aa2e4256ece473749cd51605f579b6a6f405
applepies7 <applepies@gmail.com>
2020-02-14 11:18:27 +0900

    aaaa

---
+++ jQuery/serverSide.php
@@ -0,0 +1,9 @@
+<?php
+$v_name = $_POST["nm_name"];
+$v_age = $_POST["nm_age"];
+$v_sb = $_POST["nm_sb"];
+$v_etc = $_POST["nm_etc"];
+
+echo " 넘겨받은 값은".$v_name." ".$v_age." ".$v_sb." ".$v_etc."일껄?";
+
+?>
